c3bd6708b5a3106ad0cb749d00fba9cbdd302166
Prince Kumar Singh <princekumar2000.pks@gmail.com>
2023-03-31 02:23:55 +0530

    Added home route

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const app = require('./app')
 // importing the environment variables
 require('dotenv').config()
 
+app.get('/', (req, res) => {
+    res.send('Hello to the todo app')
+})
 // starting the server  on port 3000 
 app.listen(process.env.PORT, () => {
     console.log('Server is up on port ' + process.env.PORT)
